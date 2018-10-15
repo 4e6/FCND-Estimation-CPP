894fb3ac82bb495ddd60a4c3c8f45d0b65fb07ff
Dmitry Bushev <bushevdv@gmail.com>
2018-10-15 20:03:27 +0300

    Pass step 2. Attitude estimation

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -93,9 +93,16 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   // (replace the code below)
   // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+  Quaternion<float> quat = Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+  quat = quat.IntegrateBodyRate(gyro, dtIMU);
+
+  float predictedPitch = quat.Pitch();
+  float predictedRoll = quat.Roll();
+  ekfState(6) = quat.Yaw();
+
+  // float predictedPitch = pitchEst + dtIMU * gyro.y;
+  // float predictedRoll = rollEst + dtIMU * gyro.x;
+  // ekfState(6) = ekfState(6) + dtIMU * gyro.z;    // yaw
 
   // normalize yaw to -pi .. pi
   if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
